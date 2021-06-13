aea8fc674ab4a8e7fd1530d0db98cacf96662afb
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-06-14 00:32:23 +0200

    displaying images of meals

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app_navigation/data/dummy_data.dart';
+import 'package:meals_app_navigation/widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -8,13 +10,29 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where(
+          (cat) => cat.categories.contains(categoryId),
+        )
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: Center(
         child: ListView.builder(
-          itemBuilder: (context, index) {},
+          itemCount: categoryMeals.length,
+          itemBuilder: (context, index) {
+            return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration,
+            );
+          },
         ),
       ),
     );
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app_navigation/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  const MealItem({
+    this.title,
+    this.imageUrl,
+    this.duration,
+    this.complexity,
+    this.affordability,
+  });
+
+  void _selectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    width: double.infinity,
+                    height: 250,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
